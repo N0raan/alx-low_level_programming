ea67bc849bc30fe5e2c7d989a5f597af86450331
Noran <noranssayed@gmail.com>
2023-04-28 17:31:07 -0700

    edits

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,10 @@
 * Return: 0
 */
 
-int main(void)
+int _isupper(int c)
 {
-char c;
-c = 'A';
-printf("%c: %d\n", c, _isupper(c));
-c = 'a';
-printf("%c: %d\n", c, _isupper(c));
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
 return (0);
 }
