1cc92427e48e5f824c3b1102deddbc286761068f
Noran <noranssayed@gmail.com>
2023-05-02 15:20:05 -0700

    method to swap between two variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio>
+#include "main.h"
+
+/**
+* swap_int - swap between two integers
+* @a : pointer integer
+* @b : pointer integer
+**/
+
+void swap_int(int *a, int *b)
+{
+int p = *a;
+*a = *b;
+*b = p;
+}
