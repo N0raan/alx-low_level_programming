53140dd13315545f4806b36fa275bc42d2dd5719
Noran <noranssayed@gmail.com>
2023-05-02 15:35:20 -0700

    method to return length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen - return string length
+* @s :pointer string
+* Return: string length
+*/
+
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+int sum;
+sum = sum + i;
+}
+return (sum);
+}
