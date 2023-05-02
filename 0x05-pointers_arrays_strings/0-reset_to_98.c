dc9c07336a871ccb1aa8c7ec4e199ac2a1dae730
Noran <noranssayed@gmail.com>
2023-05-02 15:02:57 -0700

    a method that set variable value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
+/**
 * reset_to_98
 * description: this method set 98 as value for the variable
 * Always return = 0
-**/
+*/
 
 void reset_to_98(int *n)
 {
