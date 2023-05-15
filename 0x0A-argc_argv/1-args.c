6dedfcbd7a5c3bd13f395c4ab6e17add85ddcf30
Noran <noranssayed@gmail.com>
2023-05-15 14:50:50 -0700

    edits

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print a number of argument passed into it
+ * @argc : integer counter
+ * @argv: char arguments values
+ * @i : integer loop counter
+ * Return: 0 in success
+ */
+
+int main(int argc, char *argv)
+{
+	int i;
+	for(i=1; i < argc ; i++)
+	{
+		printf("%s /n",argv[i]);
+	}
+	return(0);
+}
