8fa6b716a134e85cc22ea51cf4fa7ea94a649411
Noran <noranssayed@gmail.com>
2023-05-18 16:36:42 -0700

    all at once

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - print a number of argument passed into it
- * @argc : integer counter
- * @argv: char arguments values
- * @i : integer loop counter
- * Return: 0 in success
+ * main -  prints the number of arguments passed into it
+ * @argc: number of command line arguments
+ * @argv: pointer to an array of command line arguments
+ * Return: 0
  */
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-	int i;
-	for (i = 1; i < argc ; i++)
-	{
-		printf("%s /n", argv[i]);
-	}
+	(void)argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
